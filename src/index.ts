de6c30dc9d91974c585c5b0037a9f8128307da35
Ashraf Sayed <ashraf.neizk@gmail.com>
2022-10-15 09:21:24 +0200

    install sharp

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+// imports
 import express from 'express';
+import { promises as fs } from 'fs';
+// initialize app
 const app = express();
 const port = 3000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-})
+});
+app.get('/api', (req, res) => {
+    res.send('hello world');
+});
